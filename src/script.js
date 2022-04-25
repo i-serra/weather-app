98d9f166df02f8fc88dcba3f11c105a47445c4fe
i-serra <104382671+i-serra@users.noreply.github.com>
2022-04-25 18:38:57 +0100

    Created script.js

---
+++ src/script.js
@@ -0,0 +1,127 @@
+function showCurrentTime() {
+  let now = new Date();
+  let date = now.getDate();
+  let day = document.querySelector("#day");
+  day.innerHTML = `${date}`;
+  let months = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+  ];
+  let month = months[now.getMonth()];
+  let currentMonth = document.querySelector("#month");
+  currentMonth.innerHTML = `${month}`;
+
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuedsday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDayOfWeek = daysOfWeek[now.getDay()];
+  let weekDay = document.querySelector("#day-of-week");
+  weekDay.innerHTML = `${currentDayOfWeek},`;
+
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let hour = document.querySelector("#hour");
+  hour.innerHTML = `${hours}h`;
+
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentMinutes = document.querySelector("#minutes");
+  currentMinutes.innerHTML = `${minutes}`;
+}
+console.log(showCurrentTime());
+
+function displayCity(cityInput) {
+  let apiKey = "4eb9092a1ec1063ec22057d44d0bacc8";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showCurrentTemp);
+}
+
+function searchingCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#enter-city").value;
+  displayCity(cityInput);
+}
+
+let form = document.querySelector("#search-city");
+
+form.addEventListener("submit", searchingCity);
+
+function showCurrentTemp(response) {
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity} %`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  document.querySelector(
+    "#description"
+  ).innerHTML = `Preview: ${response.data.weather[0].main}`;
+  document.querySelector("#city").innerHTML = `${response.data.name}, `;
+}
+
+function showCurrentLocation(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "4eb9092a1ec1063ec22057d44d0bacc8";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showCurrentTemp);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showCurrentLocation);
+}
+
+document
+  .querySelector("#current-location")
+  .addEventListener("click", getCurrentPosition);
+
+displayCity("Lisbon");
+
+//function showTemperatureFahrenheit(event) {
+// event.preventDefault();
+// let temperatureElement = document.querySelector("#temperature");
+// let temperature = temperatureElement.innerHTML;
+//temperatureElement.innerHTML = ` ${Math.round((temperature * 9) / 5 + 32)}`;
+//}
+
+//let change = document.querySelector("#fahrenheit");
+
+//change.addEventListener("click", showTemperatureFahrenheit);
+
+//function showTemperatureCelsius(event) {
+// event.preventDefault();
+// let temperatureCelsius = document.querySelector("#temperature");
+// temperatureCelsius.innerHTML = temp;
+//}
+
+//let changeBack = document.querySelector("#celsius");
+
+//changeBack.addEventListener("click", showTemperatureCelsius);
