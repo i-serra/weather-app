1312f7c7fbf99a35aa4cf984471248996e78a67d
i-serra <104382671+i-serra@users.noreply.github.com>
2022-05-14 09:14:42 +0100

    Started working on the api call

--- src/script.js
+++ src/script.js
@@ -45,6 +45,14 @@ function searchingCity(event) {
   displayCity(cityInput);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "4eb9092a1ec1063ec22057d44d0bacc8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units={metric}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showCurrentTemp(response) {
   let iconElement = document.querySelector("#main-icon");
   celsiusTemperature = Math.round(response.data.main.temp);
@@ -54,6 +62,7 @@ function showCurrentTemp(response) {
   document.querySelector("#description").innerHTML = `Preview: ${response.data.weather[0].main}`;
   document.querySelector("#city").innerHTML = `${response.data.name}`;
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  getForecast(response.data.coord);
 }
 
 function showCurrentLocation(position) {
@@ -88,7 +97,8 @@ function showCelsiusTemperature(event) {
   temperatureCelsius.innerHTML = celsiusTemperature;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
